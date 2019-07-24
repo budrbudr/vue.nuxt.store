93b931b7849fc8909eef60bc5d195969aaa5b6a0
Budr <budr245@gmail.com>
2019-07-24 16:56:29 +0300

    table with sort and delete comments

---
+++ commetnsVue/components/Table.vue
@@ -0,0 +1,54 @@
+<template>
+
+    <div class="container">
+      <b-table bordered striped  :fields="fields" :items="news" >
+
+      </b-table>
+    </div>
+</template>
+
+<script>
+
+  export default {
+    data() {
+      return {
+        fields: [
+          {
+            key: 'title',
+            sortable: true
+          },
+          {
+            key: 'content',
+            sortable: true
+          },
+          {
+            key: 'comments',
+            sortable: true
+          },
+          {
+            key: 'Actions',
+            sortable: true
+          },
+        ],
+      }
+    },
+
+        async asyncData({$axios}) {
+          let news = [];
+          try {
+            news = await $axios.$get('/comments.json');
+          } catch (e) {
+            console.warn(e)
+          }
+          return {
+            news
+          }
+        }
+
+
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ commetnsVue/pages/index.vue
@@ -0,0 +1,62 @@
+<template>
+<div class="container">
+  <h3 class="vue-title ">Articles</h3>
+
+
+
+  <div class="container">
+    <b-table bordered striped  :fields="fields" :items="news" >
+
+    </b-table>
+  </div>
+</div>
+</template>
+
+<script>
+
+
+import Table from "../components/Table";
+export default {
+  components: {
+    Table
+  },
+  data() {
+    return {
+      fields: [
+        {
+          key: 'title',
+          sortable: true
+        },
+        {
+          key: 'content',
+          sortable: true
+        },
+        {
+          key: 'comments',
+          sortable: true
+        },
+        {
+          key: 'Actions',
+          sortable: true
+        },
+      ],
+    }
+  },
+
+  async asyncData({$axios}) {
+    let news = [];
+    try {
+      news = await $axios.$get('/comments.json');
+    } catch (e) {
+      console.warn(e)
+    }
+    return {
+      news
+    }
+  }
+}
+methods: {
+
+}
+</script>
+
